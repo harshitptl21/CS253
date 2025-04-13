0fb149652c97075b89df9a18e941c70a37b2a87a
sangabadri <badrisanga26>
2025-04-13 12:00:01 +0530

    Changing email in footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <footer class=footer>
-	<p>For any queries, please feel free to contact us at <a href="mailto:email"
-            style="color: blue;">[Email]</a> or call us
-        at <a style="color: blue">[Phone Number]</a>. We're here to assist you with any questions or
+	<p>For any queries, please feel free to contact us at <a href="mailto:team20@gmail.com"
+            style="color: blue;">team20@gmail.com</a> or call us
+        at <a href="tel:1234567890" style="color: blue">1234567890</a>. We're here to assist you with any questions or
         concerns you might have.
 	</p>
 	<p>&copy;CS253: Ride With Us</p>
